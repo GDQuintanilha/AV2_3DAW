5495f0db87de968cc549b78ac0d3e2e2764f85a7
GDQuintanilha <guidquintanilha@gmail.com>
2026-07-06 09:57:39 -0300

    Update agencias.php

--- backend/agencias.php
+++ backend/agencias.php
@@ -1,11 +1,11 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
 require_once 'config/db.php';
 
 $pdo = getConnection();
-$stmt = $pdo->query('SELECT id, nome, endereco, latitude, longitude FROM agencias');
+$stmt = $pdo->query('SELECT id, nome, endereco FROM agencias');
 $agencias = $stmt->fetchAll();
 
-echo json_encode($agencias);
+echo json_encode($agencias, JSON_UNESCAPED_UNICODE);
